33d430dd47045e305bafbfbf320c62a8211cd752
Vitor <you@example.com>
2023-05-25 09:28:32 -0300

    feat: implementa index para cada task adicionada

--- list_task.dart
+++ list_task.dart
@@ -1,15 +1,19 @@
 class MyList {
-  List<Map<dynamic, String>> tasks = [];
+  List<Map<String, dynamic>> tasks = [];
+  late int index = 0;
 
   add_task(task) {
-    tasks.add({"Title": task.title, "Description": task.description});
+    index++;
+    tasks.add(
+        {"index": index, "Title": task.title, "Description": task.description});
   }
 
   show_tasks() {
     tasks.forEach((element) {
       String? title = element['Title'];
       String? description = element['Description'];
-      print("\n$title\n- $description");
+      int? index = element["index"];
+      print("\n$index - $title:\n $description\n");
     });
   }
 }
